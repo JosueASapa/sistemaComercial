535375ef2ed47ae14e467020ebdbdf60cc9d8bb6
Martin <martinmzp19@gmail.com>
2024-06-25 11:55:27 -0700

    merge con alan

---
+++ app/Models/DatosFiscales.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DatosFiscales extends Model
+{
+    use HasFactory;
+}
